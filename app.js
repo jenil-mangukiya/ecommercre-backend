e652fd3e5da84262f1aa16668f321b881bb9e7c7
jenil-mangukiya <jenilmangukiya802@gmail.com>
2025-02-06 16:14:08 +0530

    inital commit

--- app.js
+++ app.js
@@ -12,11 +12,7 @@ var usersRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
 var addtocartRouter = require("./routes/addtocart")
 var checkoutRouter = require("./routes/checkout")
-const allowedOrigins = [
-  "https://mahirstones.com",
-  "https://admin.mahirstones.com",
-  "https://api.mahirstones.com"
-];
+
 var app = express();
 
 
@@ -35,14 +31,7 @@ console.log(cloudinary.config().secure);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
+  origin: '*',
   optionsSuccessStatus: 200
 }))
 app.use(logger('dev'));
